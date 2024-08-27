9567240089d54f6a7ceac73157afc08a1ef2c22d
biaep <00001118956680sp@al.educacao.sp.gov.br>
2024-08-27 14:01:34 -0300

    salvando projeto

---
+++ sketch.js
@@ -0,0 +1,15 @@
+let cor;
+
+function setup() {
+  background('#C562DB')
+  createCanvas(600, 400);
+  
+}
+
+function draw() {
+  cor=color(random (0,255),random(0,255), random (0,255))
+   stroke(' black')
+  fill(cor);
+  \\console.log(mouseIspressed);n
+  
+}
